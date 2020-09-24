7e1629f46089cfc4dd8076b90ac7d8dbedeab765
Ramon Carceles <juanramoncarceles@gmail.com>
2020-09-24 23:25:29 +0200

    Change on formatPriceFactory to remove space between price number and symbol.

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,16 +1,19 @@
-import type { BaseObject } from '../types';
+import type { BaseObject } from "../types";
 
 /**
  * Merges two arrays of objects by adding to the first one the objects of the second one
  * only if they don't exist by comparing their id field.
  * @returns A new array (shallow copy) with the merged contents.
  */
-export function mergeArraysOfObjects<T extends BaseObject>(arr1: Array<T>, arr2: Array<T>) { // {id: string | number}
+export function mergeArraysOfObjects<T extends BaseObject>(
+  arr1: Array<T>,
+  arr2: Array<T>
+) {
+  // {id: string | number}
   const finalArr = [...arr1];
-  arr2.forEach(el => {
-    if (finalArr.findIndex(fEl => fEl.id === el.id) === -1)
-      finalArr.push(el);
-  })
+  arr2.forEach((el) => {
+    if (finalArr.findIndex((fEl) => fEl.id === el.id) === -1) finalArr.push(el);
+  });
   return finalArr;
 }
 
@@ -18,7 +21,7 @@ export function mergeArraysOfObjects<T extends BaseObject>(arr1: Array<T>, arr2:
  * Creates a function that formats the price based on some options.
  */
 export function formatPriceFactory(decimals: number, currencySymbol: string) {
-  return function(priceNum: number) {
-    return `${priceNum.toFixed(decimals)} ${currencySymbol}`;
-  }
+  return function (priceNum: number) {
+    return `${priceNum.toFixed(decimals)}${currencySymbol}`;
+  };
 }
