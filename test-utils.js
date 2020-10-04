3c8e20c0dd8f8034886a6c0f94add9a0f2baa290
Ramon Carceles <juanramoncarceles@gmail.com>
2020-10-04 23:25:01 +0200

    Added and configured Jest.

---
+++ test-utils.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import { primary, typeScale } from "./styles";
+
+const theme = {
+  primary,
+  typeScale,
+};
+
+const AllTheProviders = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
