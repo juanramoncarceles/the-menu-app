e9e424452eee1bfdd9c393e348d6de9fe83bfe9a
Ramon Carceles <juanramoncarceles@gmail.com>
2020-10-11 21:35:41 +0200

    Added SVG icons as React components.

---
+++ components/Icons.tsx
@@ -0,0 +1,17 @@
+export const CloseIcon = () => (
+  <svg viewBox="0 0 24 24" height="24" width="24">
+    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+  </svg>
+);
+
+export const AddIcon = () => (
+  <svg viewBox="0 0 24 24" height="24" width="24">
+    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
+  </svg>
+);
+
+export const SubtractIcon = () => (
+  <svg viewBox="0 0 24 24" height="24" width="24">
+    <path d="M5 13h14v-2h-14z"/>
+  </svg>
+);
