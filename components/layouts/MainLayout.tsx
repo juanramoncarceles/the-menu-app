0a19f33aa1e362ff265f3071fddf496766d8e704
Ramon Carceles <juanramoncarceles@gmail.com>
2020-10-21 23:58:21 +0200

    Current language is set in AppContext from the MainLayout to affect all pages using it.

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,17 +1,30 @@
-import { ReactNode, useContext } from 'react';
+import { ReactNode, useContext, useEffect } from 'react';
 import Head from 'next/head';
+import { useRouter } from "next/router";
+
 import { StateContext } from "../../contexts/AppContext";
 import { ThemeProvider } from "styled-components";
 import LanguageSelect from '../Language';
 import ThemeSelect from "../ThemeSelect";
 import { GlobalStyle } from '../../styles';
+import { ActionTypes } from "../../types/enums";
+import { DispatchContext } from "../../contexts/AppContext";
+import { isLocale } from "../../translations/types";
 
 interface IProps {
   children?: ReactNode;
 }
 
 const MainLayout = ({children}: IProps) => {
-  const { theme } = useContext(StateContext);
+  const { theme, locale } = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
+  const { query } = useRouter();
+
+  useEffect(() => {
+    if (typeof query.lang === "string" && isLocale(query.lang) && locale !== query.lang) {
+      dispatch({ type: ActionTypes.ChangeLanguage, payload: query.lang });
+    }
+  }, [query.lang, locale]);
 
   return (
     <ThemeProvider theme={theme}>
